2e2173c8cc8ba1a88de8226bd8434b2fa22bd9d1
Chai <developer.chaitra@gmail.com>
2023-01-12 20:52:55 +1300

    building the board

--- game-app/src/Game.js
+++ game-app/src/Game.js
@@ -9,6 +9,24 @@ const Game = () => {
       <div className='board'>
         <div className='row'>
           <Square val={board[0]} />
+
+          <Square val={board[1]} />
+
+          <Square val={board[2]} />
+        </div>
+        <div className='row'>
+          <Square val={board[3]} />
+
+          <Square val={board[4]} />
+
+          <Square val={board[5]} />
+        </div>
+        <div className='row'>
+          <Square val={board[6]} />
+
+          <Square val={board[7]} />
+
+          <Square val={board[8]} />
         </div>
       </div>
     </div>
